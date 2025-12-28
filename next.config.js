d55e01e3023375460f0de2e0000845ced418c079
sussybocca <babyyodacutefry@gmail.com>
2025-12-28 11:04:03 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-export default {
-  experimental: { appDir: true }
+/** @type {import('next').NextConfig} */
+import path from "path";
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
 };
+
+export default nextConfig;
