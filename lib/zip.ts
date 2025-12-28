3ebbecb4a029a602cbbf3ebbdbc11a5d5e628f19
sussybocca <babyyodacutefry@gmail.com>
2025-12-28 11:15:10 -0600

    Update zip.ts

--- lib/zip.ts
+++ lib/zip.ts
@@ -2,18 +2,28 @@ import JSZip from "jszip";
 
 export async function readZip(file: File) {
   const zip = await JSZip.loadAsync(file);
+
+  // Find main JS file
   const mainJsFile = zip.file("main.js");
-  if (!mainJsFile) throw new Error("main.js not found in zip");
+  if (!mainJsFile) throw new Error("main.js not found in ZIP");
 
   const code = await mainJsFile.async("string");
 
   // Collect all other files as assets
   const assets: Record<string, ArrayBuffer> = {};
+  const assetPromises: Promise<void>[] = [];
+
   zip.forEach((relativePath, zipEntry) => {
-    if (zipEntry.name !== "main.js") {
-      assets[zipEntry.name] = zipEntry.async("arraybuffer") as any;
+    if (zipEntry.name !== "main.js" && !zipEntry.dir) {
+      const p = zipEntry.async("arraybuffer").then((data) => {
+        assets[zipEntry.name] = data;
+      });
+      assetPromises.push(p);
     }
   });
+
+  // Wait for all assets to finish loading
+  await Promise.all(assetPromises);
 
   return { code, assets };
 }
