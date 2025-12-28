8a1a39ea29b741ac8d130f10620fd141feb2bd4a
sussybocca <babyyodacutefry@gmail.com>
2025-12-28 11:05:13 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-/** @type {import('next').NextConfig} */
 import path from "path";
+import { fileURLToPath } from "url";
+
+/** @type {import('next').NextConfig} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   reactStrictMode: true,
